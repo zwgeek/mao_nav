b6d2224f49a300eb10a214134d55aa7fd1b899ce
张小炜 <360542882@qq.com>
2025-12-18 14:50:44 +0800

    chore: 更新导航数据 - 2025/12/18 14:50:43

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -302,23 +302,23 @@ export const mockData = {
     },
     {
       "id": "entertainment",
-      "name": "娱乐休闲",
+      "name": "AI工具",
       "icon": "🎮",
       "order": 6,
       "sites": [
         {
           "id": "bilibili",
-          "name": "哔哩哔哩",
-          "url": "https://www.bilibili.com",
-          "description": "弹幕视频网站",
-          "icon": "/sitelogo/www.bilibili.com.ico"
+          "name": "Civitai",
+          "url": "https://civitai.com",
+          "description": "stable-diffusion模型资源库",
+          "icon": "https://www.faviconextractor.com/favicon/civitai.com"
         },
         {
           "id": "zhihu",
-          "name": "知乎",
-          "url": "https://www.zhihu.com",
-          "description": "知识问答社区",
-          "icon": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='black'%3E%3Cpath d='M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.01.08-2.11 0 0 .67-.21 2.2.82A7.65 7.65 0 018 4.58c.68.003 1.36.092 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.91.08 2.11.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0016 8c0-4.42-3.58-8-8-8z'/%3E%3C/svg%3E"
+          "name": "吐司",
+          "url": "https://tusi.cn",
+          "description": "stable-diffusion模型资源库",
+          "icon": "https://www.faviconextractor.com/favicon/tusi.cn"
         }
       ]
     },
